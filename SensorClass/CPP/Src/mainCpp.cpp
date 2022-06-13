7ed933ade3610979246282b00102727cadade91f
Marcos Andre <marcos32andre@gmail.com>
2022-06-12 21:13:50 -0300

    Atualizações do dia 12/06/22

--- SensorClass/CPP/Src/mainCpp.cpp
+++ SensorClass/CPP/Src/mainCpp.cpp
@@ -5,10 +5,14 @@ SENSOR sensor_Direita(Sensor_Dir_GPIO_Port, Sensor_Dir_Pin);
 SENSOR sensor_Centro(Sensor_Cen_GPIO_Port, Sensor_Cen_Pin);
 SENSOR sensor_Esquerda(Sensor_Esq_GPIO_Port, Sensor_Esq_Pin);
 
+int sensor1;
+int sensor2;
+int sensor3;
+
 void mainCpp() {
-	sensor_Direita.SensorRead();
-	sensor_Centro.SensorRead();
-	sensor_Esquerda.SensorRead();
+	sensor1 = sensor_Direita.SensorRead();
+	sensor2 = sensor_Centro.SensorRead();
+	sensor3 = sensor_Esquerda.SensorRead();
 }
 
 
